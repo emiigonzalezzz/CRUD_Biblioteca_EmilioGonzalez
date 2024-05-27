fbcca8cd07bf5cd239bac6d1822674ffbe8e3a88
emiigonzalezzz <emiigonzalezzz11@gmail.com>
2024-05-27 17:34:38 -0300

    HOLAAA

---
+++ BackEnd/Conexion/Conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+function connection (){
+    $host = "localhost";
+    $usr = "root";
+    $pass = "";
+    $bd = "biblioteca";
+    $puerto = 3306;
+    $mysqli = new mysqli ($host, $usr, $pass, $bd, $puerto);
+    return $mysqli;
+}
+?>
